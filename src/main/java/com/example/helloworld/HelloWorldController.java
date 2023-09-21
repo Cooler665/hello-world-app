e32f709733ac3e6e8aa5504c09c4b3f393a1406b
cooler <coolerdody@gmail.com>
2023-09-21 12:57:33 +0400

    after add kafka to project and error handler

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,14 +1,34 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @GetMapping("/hello")
     public String hello() {
-        // Здесь будет логирование, которое позже отправляется в Kafka
-        System.out.println("Hello, world!");
-        return "Hello, world!";
+        try {
+            // Логируем успешное сообщение
+            System.out.println("Hello, world!");
+            kafkaTemplate.send("hello-topic", "Hello, world!");
+
+            return "Hello, world!";
+        } catch (Exception e) {
+            // Отправляем ошибку в Kafka
+            sendErrorToKafka(e);
+
+            return "An error occurred";
+        }
+    }
+
+    public void sendErrorToKafka(Exception e) {
+        String errorMessage = "Error occurred: " + e.getMessage();
+        kafkaTemplate.send("error-topic", errorMessage);
     }
 }
